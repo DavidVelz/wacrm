1b00e303577c3cf5695abcebe08d23561c35db31
David Vélez <40278117+DavidVelz@users.noreply.github.com>
2026-07-31 09:11:58 -0500

    Update page.tsx

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -32,7 +32,7 @@ export default function ForgotPasswordPage() {
     // });
 
     if (error) {
-      setError(error.message);
+      setError(error);
       setLoading(false);
       return;
     }
